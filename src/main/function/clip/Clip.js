926d1af2d5121fa46ce822ef920d5afd7392c021
김현호 <hhkim@mqni.com>
2026-03-06 10:43:12 +0900

    리스트 name host 값에서 title 값으로 변경

--- src/main/function/clip/Clip.js
+++ src/main/function/clip/Clip.js
@@ -1,6 +1,13 @@
 import { BrowserWindow } from 'electron'
 import clipboard from 'electron-clipboard-extended'
-import { saveSiteInfo, isHostExist, isUrlExist, getMinIdByHost } from '../../../service/SiteInfoService'
+import cheerio from 'cheerio'
+
+import {
+  saveSiteInfo,
+  isHostExist,
+  isUrlExist,
+  getMinIdByHost
+} from '../../../service/SiteInfoService'
 clipboard.startWatching()
 
 const initialText = clipboard.readText()
@@ -9,7 +16,7 @@ if (initialText) {
   console.log('Initial clipboard:', initialText)
 }
 
-clipboard.on('text-changed', () => {
+clipboard.on('text-changed', async () => {
   let currentText = clipboard.readText()
 
   if (currentText.includes('http')) {
@@ -18,13 +25,14 @@ clipboard.on('text-changed', () => {
     if (isUrlExist(currentText) === 1) return // 같은 URL이 이미 존재하면 저장하지 않음
 
     let parentId = null
-
+    const title = await getWebTitle(currentText)
+    const name = title ? title : currentText
     if (isHostExist(url.host) === 0) {
-      parentId = saveSiteInfo(null, url.host, currentText, url.host) // parent directory
+      parentId = saveSiteInfo(null, name, currentText, url.host) // parent directory
     } else {
       parentId = getMinIdByHost(url.host)
     }
-    saveSiteInfo(parentId, url.host, currentText, url.host) // child site
+    saveSiteInfo(parentId, name, currentText, url.host) // child site
 
     const win = BrowserWindow.getAllWindows()[0]
     if (win) {
@@ -32,3 +40,10 @@ clipboard.on('text-changed', () => {
     }
   }
 })
+
+async function getWebTitle(url) {
+  const res = await fetch(url)
+  const html = await res.text()
+  const $ = cheerio.load(html)
+  return $('title').text()
+}
